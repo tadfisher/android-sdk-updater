b3e3ce63fcad99d40387f0e8ea931b10f5950b40
Tad Fisher <tadfisher@gmail.com>
2016-07-24 15:56:08 -0700

    Bump changelog and version

--- sdk_updater/__init__.py
+++ sdk_updater/__init__.py
@@ -1,12 +1,12 @@
 """
 android-sdk-updater
 
-:copyright: (c) 2015 Tad Fisher
+:copyright: (c) 2016 Tad Fisher
 :license: Apache 2.0, see LICENSE for more details.
 
 """
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __author__ = 'Tad Fisher'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Tad Fisher'
